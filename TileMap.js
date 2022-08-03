2ba65d812dfc8c1ff744553f18b7409ac90b150f
cmomie <cmomie1@gmail.com>
2022-08-02 23:01:12 -0400

    Updates

--- TileMap.js
+++ TileMap.js
@@ -2,7 +2,7 @@ export default class TileMap {
     constructor(tileSize){
         this.tileSize = tileSize; //implement the constructor and pass the tile size with by setting a variable for it
         this.wall = this.#image('wall.png');
-        this.pacman = this.#image('pac0.png');
+        this.pacman = this.#image('pac1.png');
         this.dot = this.#image('yellowDot.png');
         this.ghost = this.#image('ghost.png');
 
@@ -25,33 +25,63 @@ export default class TileMap {
     map =[
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,0,2,0,0,0,0,0,0,0,0,0,0,3,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,1,1,1,1,1,1,1,1,1,1,1,0,1],
+        [1,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,1,0,1,1,1,1,1,1,1,1,1,0,1],
+        [1,0,1,0,1,0,0,3,0,0,0,0,0,0,1],
+        [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
+        [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
+        [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
+        [1,3,0,0,0,0,0,0,0,0,0,0,0,3,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
     //method that takes in canvas and ctx to draw the game again during the loop
     draw(canvas, ctx) {
         this.#setCanvasSize(canvas);
         this.#clearCanvas(canvas, ctx);
-        this.drawMap(ctx);
+        this.#drawMap(ctx);
     }; 
-    #clearCanvas(canvas, ctx){
-        ctx.fillStyle = ('black');
-        ctx.fillReact = (0, 0, canvas.width, canvas.height);
+
+    #drawMap(ctx){
+        for (let row = 0; row < this.map.length; row++) {
+            for (let column = 0; column < this.map[row].length; column++) {
+                const tile = this.map[row] [column];
+                let image = null;
+                switch (tile) {
+                    case 1:
+                        image = this.wall;
+                        break;
+                    case 0:
+                        image = this.dot;
+                        break;
+                    case 2:
+                        image = this.pacman;
+                        break;
+                    case 3:
+                        image = this.ghost;
+                        break;
+                }
+               if (image != null)
+                ctx.drawImage(
+                    image, 
+                    column * this.tileSize, 
+                    row * this.tileSize, 
+                    this.tileSize,
+                    this.tileSize
+                    );
+            }
+        }
+
+    }
+
+    #clearCanvas(canvas, ctx) {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
    
-    #setCanvasSize(canvas){
+    #setCanvasSize(canvas) {
     canvas.height = this.map.length * this.tileSize;
     canvas.width = this.map[0].length * this.tileSize; 
-   };  
-   //double check to ensure that this method needs to be added
-   drawMap(ctx) {
-
    }
    
 }
@@ -73,3 +103,6 @@ export default class TileMap {
 //create a method called clear the canvas inside of the draw method that takes in the same properties as draw 
 //define that function/method and put in the same properties on line
 //give the canvas a fill style and color of black and use x and y axis on  the fill react. 
+
+//Create a for loop to draw the map by looping the rows in the map by getting the length line 44
+//after looping the rows loop the columns
