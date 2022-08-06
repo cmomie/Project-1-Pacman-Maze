1b499940a2e4bd30dc3a70def0844718003fae98
cmomie <cmomie1@gmail.com>
2022-08-06 11:41:15 -0500

    Project Updates

--- ReadMe.md
+++ ReadMe.md
@@ -1,12 +1,19 @@
-                            Create Files Needed
-    images directory with the pictures of the players and tile map, include css styling in html file, game.js, tilemap.js, index.html
-                            Define the varialbes needed to create the game display
+******************** Objective of the Game ***********************
+Get Pacman through the maze until he reaches the Ghost in under 30 seconds
+                        
+                            
+******************** Create Files Needed ***********************
+    images directory with the pictures of the pacman players and tile map, include css styling in html file, game.js, tilemap.js, index.html
+                            
 
                             Create the function loop that will allow the game to operate
 
-                            Create the character legend and 2d gameboard
+                            
 
-    // Step 2 make the grid using a 2 demensional array (and array with other arrays inside of it) 
+******************** Game Creation ************************
+    //Create the character legend and 2d gameboard
+    //Make the grid using a 2 demensional array 
+    //(and array with other arrays inside of it) 
     //this is how I identify how many rows(the number of arrays inside the outer array) 
     //the columns is determined by the number of items in each inner arrays
     //created a legend to identify the part of the game board and players
@@ -14,14 +21,30 @@
     //0 = dots
     //2 = pacman
     //3 = enemies
+    //Add methods and functions to display the gameboard and player on the screen
 
-Add methods and functions to display the gameboard and players on the screen
+    //Define the varialbes needed to create the game display
+    //Get images to display 
+    //use individual images to get this game to display on the screen 
+    //load the images using a helper method #image(using the hash lets me know it's a private method) 
+    // give it the name fileName
+    //create an image object and set a path using src and path to the images using a string
+    //next load the images in the constructor created with the class TileMap
+    //load images by defining the 1st image, the wall using this. and call that helper method with the file name 
+
+    //Set images sizes
+    //Create a new method inside the draw method called setCanvasSize(canvas) to take in the method inside of the draw method
+    //then implement setCanvasSize and set the height and width lines 42-44 
+    //height is based on the number of items from the array, the width comes from the number of columns using the 1st row in the array
+
+    //Clear the canvas
+    //create a method called clear the canvas inside of the draw method that takes in the same properties as draw 
+    //define that function/method and put in the same properties on line
+    //give the canvas a fill style and color of black and use x and y axis on  the fill rect. 
+
+    //Create a for loop to draw the map by looping the rows in the map by getting the length line 44
+    //after looping the rows loop the columns
 
 source used to inspire game: https://youtu.be/Tk48dQCdQ3E
 
 
-**********Obstacles : the game board in not black according to the code on line 44 of the TileMap.js file. 
-I was able to increase the size of my gameboard. 
-I plan to continue to work on adding the players to my board. 
-
-I'm confused about how the images are in two different places pacman.js and tileMap.js I think they one overides the other
--- game.js
+++ game.js
@@ -1,9 +1,25 @@
+class Character {
+    constructor(x, y, image, width, height) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.alive = true;
+
+        this.render = function() {
+            ctx.drawImage(this.image, this.x, this.y);
+        }
+    }
+}
+
+let ghost 
 const MovingDirection = {
-    up: 0,
-    down: 1,
-    left: 2,
-    right: 3,
-};
+     up: 0,
+     down: 1,
+     left: 2,
+     right: 3,
+ };
 class Pacman {
     constructor(x, y, tileSize, velocity, tileMap){
         this.x = x;
@@ -11,7 +27,8 @@ class Pacman {
         this.tileSize = tileSize;
         this.velocity = velocity;
         this.tileMap = tileMap;
-
+        this.alive = true;
+      
         this.currentMovindDirection = null;
         this.requestedMovingDIrection = null;
 
@@ -73,10 +90,11 @@ class Pacman {
     };
     #move() {
         if(this.currentMovindDirection !== this.requestedMovingDirection) {
-            if(Number.isInteger(this.x/this.tileSize) && 
+            if(
+            Number.isInteger(this.x/this.tileSize) && //tells if we are in a positive area on the tileMap
             Number.isInteger(this.y/this.tileSize)
             ){
-                this.currentMovindDirection = this.requestedMovingDirection;
+              
             }
         }
         switch(this.currentMovindDirection) {
@@ -130,15 +148,15 @@ class TileMap {
     //5 = pinkDot
     map =[
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,2,0,4,0,0,0,0,0,0,0,0,3,1],
+        [1,0,0,0,4,0,0,0,0,0,0,0,0,0,1],
         [1,0,1,1,1,1,1,1,1,1,1,1,1,0,1],
         [1,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,1,0,1,1,1,1,1,1,1,1,1,0,1],
-        [1,0,1,0,1,0,0,3,0,0,0,0,0,0,1],
+        [1,0,1,0,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,1,0,1,0,0,3,0,0,0,0,0,1,1],
         [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
         [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
         [1,0,1,0,1,0,1,1,1,1,0,1,1,0,1],
-        [1,3,0,0,0,0,0,0,0,0,0,0,0,3,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
     //method that takes in canvas and ctx to draw the game again during the loop
@@ -187,7 +205,7 @@ class TileMap {
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
-   //started here trying to add pacman movement and the board went away
+   //started here trying to add pacman movement 
     getPacman(velocity) {
         for (let row =0; row < this.map.length; row++){
             for(let column =0; column < this.map[row].length; column++){
@@ -206,46 +224,185 @@ class TileMap {
    
 }
 
-//Get images to display 
-//use individual images to get this game to display on the screen 
-//load the images using a helper method #image(using the hash lets me know it's a private method) 
-// give it the name fileName
-//create an image object and set a path using src and path to the images using a string
-//next load the images in the constructor created with the class TileMap
-//load images by defining the 1st image, the wall using this. and call that helper method with the file name 
-
-//Set images sizes
-//Create a new method inside the draw method called setCanvasSize(canvas) to take in the method inside of the draw method
-//then implement setCanvasSize and set the height and width lines 42-44 
-//height is based on the number of items from the array, the width comes from the number of columns using the 1st row in the array
-
-//Clear the canvas
-//create a method called clear the canvas inside of the draw method that takes in the same properties as draw 
-//define that function/method and put in the same properties on line
-//give the canvas a fill style and color of black and use x and y axis on  the fill react. 
-
-//Create a for loop to draw the map by looping the rows in the map by getting the length line 44
-//after looping the rows loop the columns
-
-
-
 const tileSize = 32; //we want each tile as 32 pixels; Identify how big the tiles should be in the game using tileSize
 const velocity = 2;
 
-//Create the following variable that will be needed for the game to operate and be view on the browser.
+//Create the following variable that will be needed for the game to operate and be viewied on the browser.
 
 const canvas = document.getElementById('gameCanvas'); //1st  define canvas to find it's html element by id 'gameCanvas';
 const ctx = canvas.getContext('2d'); //define this to draw to the screen in 2d
+const status = document.getElementById('status');
+const score = document.getElementById('score'); // we went in and added an id for the score 
+const ghostimg = document.getElementById('ghost');
 //the tileMape has to be initialized after being imported on line 1 by creating a variable.  
-const tileMap = new TileMap(tileSize); //passing in the tile size in the new TileMap
-const pacman = tileMap.getPacman(velocity)
+//let tileMap 
+//let pacman 
 /* 2nd create the function gameLoop; this loop will redraw the game every second for up to 75 times using setInterval; 
 1000 miliseconds = 1 second (This Function is the Most Important Part of the Game) */
+window.addEventListener('DOMContentLoaded', function() {
+     tileMap = new TileMap(tileSize); //passing in the tile size in the new TileMap
+     pacman = tileMap.getPacman(velocity)
+    ghost = new Character(10, 20, ghostimg, 70, 80)
+    const runGame = setInterval(gameLoop, 1000 / 75);
+});
+function movementHandler(e) {
+    console.log('movement', e.key);
+//switch will allow us to use different keys according to the following conditions if we press those buttons
+    switch(e.key) {
+        case 'ArrowUp': //moves pacman up
+            pacman.y - 10 >= 0 ? (pacman.y -= 10) : null; //it will check if it's greater than or equal to 0 then if it is it will move 10 spaces
+        break; //type break to add additional cases or keys that we can press
+        case 'ArrowLeft': //moves pacman left
+            pacman.x - 10 >= 0 ? (pacman.x -= 10) : null;
+        break
+        case 'ArrowDown'://moves pacman down
+            pacman.y + 10 <= canvas.height ? (pacman.y += 10) : null;
+        break
+        case 'ArrowRight'://moves pacman right
+            pacman.x + 10 <= canvas.width ? (pacman.x += 10) : null;
+        break
+        case 'w': //moves pacman up
+        pacman.y - 10 >= 0 ? (pacman.y -= 10) : null; //it will check if it's greater than or equal to 0 then if it is it will move 10 spaces
+        break; //type break to add additional cases or keys that we can press
+        case 'a': //moves pacman left
+        pacman.x - 10 >= 0 ? (pacman.x -= 10) : null;
+         break
+        case 's'://moves pacman down
+        pacman.y + 10 <= canvas.height ? (pacman.y += 10) : null;
+        break
+        case 'd'://moves pacman right
+        pacman.x + 10 <= canvas.width ? (pacman.x += 10) : null;
+        break
+    }
+
+}
+function detectHit(player1, player2) {
+    //booleen condition is testing if all of the following is true to see if the players actually hit each other according to the players x,y, height, and width positions
+    let hitTest = 
+    player1.y + player1.height > player2.y &&
+    player1.y < player2.y + player2.height &&
+    player1.x + player1.width > player2.x &&
+    player1.x < player2.x + player2.width;
+    //if they actually touch each other there is a hit
+
+    if (hitTest) {//if this the test above is true we will add 100 points to the current score. 
+        let newScore = Number(score.textContent) + 100; //we grab the score from HTML here and add the 100 points as well; writing the word Number turns the string that is already there into a number
+        score.textContent = newScore;
+
+    
+    } else {
+        return false;
+    }
+}
+document.addEventListener('keydown', movementHandler);
 function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+   // movement.textContent = `X:${pacman.x}\nY:${pacman.y}`;
+    if (pacman.alive){
+        pacman.draw(ctx);
+let hit = detectHit(pacman, tileMap)
+    }
 tileMap.draw(canvas,ctx); // we will execute this on every single loop of the game
 pacman.draw(ctx);
+
 }
+
+
 //tileMap.setCanvasSize(canvas); // When I added this it went back to the original small tileMap size
 setInterval(gameLoop, 1000 / 75);
 /* 3rd define the tile map to draw the screen in the game loop */
 
+class Timer {
+    constructor(root) {
+      root.innerHTML = Timer.getHTML();
+  
+      this.el = {
+        minutes: root.querySelector(".timer__part--minutes"),
+        seconds: root.querySelector(".timer__part--seconds"),
+        control: root.querySelector(".timer__btn--control"),
+        reset: root.querySelector(".timer__btn--reset")
+      };
+  
+      this.interval = null;
+      this.remainingSeconds = 0;
+  
+      this.el.control.addEventListener("click", () => {
+        if (this.interval === null) {
+          this.start();
+        } else {
+          this.stop();
+        }
+      });
+  
+      this.el.reset.addEventListener("click", () => {
+        const inputMinutes = prompt("Enter number of minutes:");
+  
+        if (inputMinutes < 60) {
+          this.stop();
+          this.remainingSeconds = inputMinutes * 60;
+          this.updateInterfaceTime();
+        }
+      });
+    }
+  
+    updateInterfaceTime() {
+      const minutes = Math.floor(this.remainingSeconds / 60);
+      const seconds = this.remainingSeconds % 60;
+  
+      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+  
+    updateInterfaceControls() {
+      if (this.interval === null) {
+        this.el.control.innerHTML = `<span class="material-icons">play</span>`;
+        this.el.control.classList.add("timer__btn--start");
+        this.el.control.classList.remove("timer__btn--stop");
+      } else {
+        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        this.el.control.classList.add("timer__btn--stop");
+        this.el.control.classList.remove("timer__btn--start");
+      }
+    }
+  
+    start() {
+      if (this.remainingSeconds === 0) return;
+  
+      this.interval = setInterval(() => {
+        this.remainingSeconds--;
+        this.updateInterfaceTime();
+  
+        if (this.remainingSeconds === 0) {
+          this.stop();
+        }
+      }, 1000);
+  
+      this.updateInterfaceControls();
+    }
+  
+    stop() {
+      clearInterval(this.interval);
+  
+      this.interval = null;
+  
+      this.updateInterfaceControls();
+    }
+  
+    static getHTML() {
+      return `
+              <span class="timer__part timer__part--minutes">00</span>
+              <span class="timer__part">:</span>
+              <span class="timer__part timer__part--seconds">00</span>
+              <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+                  <span class="material-icons">play</span>
+              </button>
+              <button type="button" class="timer__btn timer__btn--reset">
+                  <span class="material-icons">timer</span>
+              </button>
+          `;
+    }
+  }
+  
+  new Timer(
+      document.querySelector(".timer")
+  );
